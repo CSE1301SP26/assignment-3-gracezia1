cae98e23164c923a724347997670119436f8d404
gracezia1 <g.a.zia@wustl.edu>
2026-02-11 14:23:34 -0600

    assignment 3 updated

--- src/assignment3/FrequencyTable.java
+++ src/assignment3/FrequencyTable.java
@@ -3,17 +3,23 @@ import java.util.Scanner;
 public class FrequencyTable {
 public static void main(String[] args){
 Scanner in = new Scanner(System.in);
+System.out.println("How many dice?");
 int n = in.nextInt();
-int[][] frequency = new int[2][n*6];
+int[][] frequency = new int[2][n*5 + 1];
+int sum = 0;
 for (int i = 0; i < frequency[0].length; i++){
     frequency[0][i] = i + n;
 }
-for (int j = 0; j < n + 1; j++){
-    int roll = ((int)(Math.random() * 6 + 1)) * n;
+for (int j = 0; j < 1000; j++){
+    sum = 0;
+    for (int r = 0; r < n; r++){
+        int roll = ((int)(Math.random() * 6 + 1));
+        sum = sum + roll;
     for (int k = 0; k < frequency[0].length; k++){
-        if (roll == frequency[0][k]){
+        if (sum == frequency[0][k]){
             frequency[1][k]++;
         }
+    }
     }
 }
 for (int r = 0; r < frequency[0].length; r++){
